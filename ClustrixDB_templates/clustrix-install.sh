6bc506133539d12d9530547e95d16f1cdde3ab77
ajblardone <ablardone@clustrix.com>
2015-08-19 15:34:17 -0700

    bug fix, re-added sql and license

--- ClustrixDB_templates/clustrix-install.sh
+++ ClustrixDB_templates/clustrix-install.sh
@@ -43,23 +43,23 @@ log_path='/mnt/resource/log'
 ########################################################
 help()
 {
-	echo "This script installs ClustrixDB on a Centos virtual machine image"
-	echo "Available parameters:"
-	echo "-n Cluster name"
-	echo "-v ClustrixDB version"
-	echo "-c Number of instances"
-	echo "-i Sequential node index (starting from 0)"
-	echo "-p Private IP address prefix"
-	echo "-k ClustrixDB license key"
-	echo "-s SQL root user password"
-	echo "-l (Indicator of the last node)"
-	echo "-h Help"
+    echo "This script installs ClustrixDB on a Centos virtual machine image"
+    echo "Available parameters:"
+    echo "-n Cluster name"
+    echo "-v ClustrixDB version"
+    echo "-c Number of instances"
+    echo "-i Sequential node index (starting from 0)"
+    echo "-p Private IP address prefix"
+    echo "-k ClustrixDB license key"
+    echo "-s SQL root user password"
+    echo "-l (Indicator of the last node)"
+    echo "-h Help"
 }
 log()
 {
-	# If you want to enable this logging, uncomment the line below and specify your logging key 
-	curl -X POST -H "content-type:text/plain" --data-binary "$(date) | ${HOSTNAME} | $1" https://logs-01.loggly.com/inputs/${LOGGING_KEY}/tag/clx-install,${HOSTNAME}
-	echo "$1"
+    # If you want to enable this logging, uncomment the line below and specify your logging key 
+    curl -X POST -H "content-type:text/plain" --data-binary "$(date) | ${HOSTNAME} | $1" https://logs-01.loggly.com/inputs/${LOGGING_KEY}/tag/clx-install,${HOSTNAME}
+    echo "$1"
 }
 
 log "Begin execution of clustrix installation script extension on ${HOSTNAME}"
@@ -78,138 +78,144 @@ while getopts :n:v:c:i:p:f:k:s:lh optname; do
   
   case $optname in
     n)  # Cluster name
-		CLUSTER_NAME=${OPTARG}
-		;;
+        CLUSTER_NAME=${OPTARG}
+        ;;
     v)  # Version to be installed
-		VERSION=${OPTARG}
-		;;
-	c) # Number of instances
-		NUM_NODES=${OPTARG}
-		;;		
-	i) # Sequential node index
-		NODE_INDEX=${OPTARG}
-		;;				
-	p) # Private IP address prefix
-		IP_PREFIX=${OPTARG}
-		;;
-	f) # form new cluster 
-		CLUSTER=${OPTARG}
-		;;	
-	k) # CLX license key
-		CLX_LICENSE=${OPTARG}
-		;;
-	s) # SQL root password
-		SQLPASS=${OPTARG}
-		;;	
+        VERSION=${OPTARG}
+        ;;
+    c) # Number of instances
+        NUM_NODES=${OPTARG}
+        ;;      
+    i) # Sequential node index
+        NODE_INDEX=${OPTARG}
+        ;;              
+    p) # Private IP address prefix
+        IP_PREFIX=${OPTARG}
+        ;;
+    f) # form new cluster 
+        CLUSTER=${OPTARG}
+        ;;  
+    k) # CLX license key
+        CLX_LICENSE=${OPTARG}
+        ;;
+    s) # SQL root password
+        SQLPASS=${OPTARG}
+        ;;  
     l)  # Indicator of the last node
-		IS_LAST_NODE=1
-		;;		
+        IS_LAST_NODE=1
+        ;;      
     h)  # Helpful hints
-		help
-		exit 2
-		;;
+        help
+        exit 2
+        ;;
     \?) #unrecognized option - show help
-		echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
-		help
-		exit 2
-		;;
+        echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+        help
+        exit 2
+        ;;
   esac
 done
 
 setup_storage()
-{	
-	#data disks 
-	# find un-configured storage device(s) to setup the data volume:
-	log "data path is: $data_path."
-	log "log path is: $log_path
-	umount $data_path
-	device='/dev/sdb'
+{   
+    #data disks 
+    # find un-configured storage device(s) to setup the data volume:
+    log "data path is: $data_path."
+    log "log path is: $log_path"
+    umount $data_path
+    device='/dev/sdb'
 
-	# Clustrix log partition size: either 15% of total space or 50GB whichever is smaller
-	totsize=$((`blockdev --getsize64 $device`))
-	totsize=$(($totsize/1024/1024/1024))
-	logsize1="$(echo "scale=4; $totsize*0.15" | bc)"
-	logsize1=${logsize1%.*}
-	logsize=$(($logsize1<50?$logsize1:50))
-	dataSize=$(($totsize-$logsize))
+    # Clustrix log partition size: either 15% of total space or 50GB whichever is smaller
+    totsize=$((`blockdev --getsize64 $device`))
+    totsize=$(($totsize/1024/1024/1024))
+    logsize1="$(echo "scale=4; $totsize*0.15" | bc)"
+    logsize1=${logsize1%.*}
+    logsize=$(($logsize1<50?$logsize1:50))
+    dataSize=$(($totsize-$logsize))
 
-	mkdir -p $data_path
-	log "Starting storage setup, single disk: $device"
-	device=$(echo $device | tr -d ' ') 
-	(echo o; echo n; echo p; echo 1; echo ; echo +"$dataSize"G; echo n; echo p; echo 2; echo; echo; echo w) | fdisk -c -u $device
-	mkfs -t ext4 $device"1"
-	e2label $device"1" CLUSTRIX-DATA
-	echo "LABEL=CLUSTRIX-DATA	$data_path	ext4	defaults,noatime,nodiratime	0 2" >> /etc/fstab
-	mount -L CLUSTRIX-DATA $data_path
-	mkfs -t ext4 $device"2"
-	e2label $device"2" CLUSTRIX-LOG
-	mkdir -p $log_path
-	echo "LABEL=CLUSTRIX-LOG	$log_path	ext4	defaults,noatime,nodiratime	0 2" >> /etc/fstab
-	mount -L CLUSTRIX-LOG $log_path
-	log "Storage setup successful"
+    mkdir -p $data_path
+    log "Starting storage setup, single disk: $device"
+    device=$(echo $device | tr -d ' ') 
+    (echo o; echo n; echo p; echo 1; echo ; echo +"$dataSize"G; echo n; echo p; echo 2; echo; echo; echo w) | fdisk -c -u $device
+    mkfs -t ext4 $device"1"
+    e2label $device"1" CLUSTRIX-DATA
+    echo "LABEL=CLUSTRIX-DATA   $data_path  ext4    defaults,noatime,nodiratime 0 2" >> /etc/fstab
+    mount -L CLUSTRIX-DATA $data_path
+    mkfs -t ext4 $device"2"
+    e2label $device"2" CLUSTRIX-LOG
+    mkdir -p $log_path
+    echo "LABEL=CLUSTRIX-LOG    $log_path   ext4    defaults,noatime,nodiratime 0 2" >> /etc/fstab
+    mount -L CLUSTRIX-LOG $log_path
+    log "Storage setup successful"
 
-	# Create symlink from /data/clustrix to /mnt/resource  
-	mkdir /data
-	cd /data
-	ln -s $data_path clustrix
+    # Create symlink from /data/clustrix to /mnt/resource  
+    mkdir /data
+    cd /data
+    ln -s $data_path clustrix
 } 
 
 tweek_os() 
 {
-	#update OS 
-	yum -y update 
+    #update OS 
+    yum -y update 
 
-	#install utils
-	yum -y install wget screen telnet
+    #install utils
+    yum -y install wget screen telnet
 
-	#turn off iptables 
-	chkconfig iptables off
-	/etc/init.d/iptables stop
+    #turn off iptables 
+    chkconfig iptables off
+    /etc/init.d/iptables stop
 }
 
 install_clx()
 {
-	log "installing CLXnode" 
+    log "installing CLXnode" 
 
-	# install specified version of clxdb (unattended) 
-	if [[ $VERSION = 'latest' ]]; 
-	then
-		export CLXSRC="http://files.clustrix.com/releases"
-		export SRCVERSION=`curl -s $CLXSRC/LATEST`
-	else
-		export CLXSRC="http://files.clustrix.com/releases/software"
-		export SRCVERSION="clustrix-$VERSION"
-	fi
+    # install specified version of clxdb (unattended) 
+    if [[ $VERSION = 'latest' ]]; 
+    then
+        export CLXSRC="http://files.clustrix.com/releases"
+        export SRCVERSION=`curl -s $CLXSRC/LATEST`
+    else
+        export CLXSRC="http://files.clustrix.com/releases/software"
+        export SRCVERSION="clustrix-$VERSION"
+    fi
 
-	curl $CLXSRC/$SRCVERSION.tar.bz2 -s | tar xvj 
-	cd $SRCVERSION; sudo -E ./clxnode_install.py --data-path=$data_path --log-path=$log_path  --ui-log-path=$log_path/clustrix_ui --yes
+    curl $CLXSRC/$SRCVERSION.tar.bz2 -s | tar xvj 
+    cd $SRCVERSION; sudo -E ./clxnode_install.py --data-path=$data_path --log-path=$log_path  --ui-log-path=$log_path/clustrix_ui --yes
 
-	# Post install steps 
-	export PATH=$PATH:/opt/clustrix/bin
-	echo 'export PATH=$PATH:/opt/clustrix/bin' >> ~clustrix/.bash_profile
-	echo 'export PATH=$PATH:/opt/clustrix/bin' >> ~root/.bash_profile
+    # Post install steps 
+    export PATH=$PATH:/opt/clustrix/bin
+    echo 'export PATH=$PATH:/opt/clustrix/bin' >> ~clustrix/.bash_profile
+    echo 'export PATH=$PATH:/opt/clustrix/bin' >> ~root/.bash_profile
 
-	# update clxnode.conf and comment out the line for backend address 
-	#sed -i "/BACKEND_ADDR/s/^/#/" /etc/clustrix/clxnode.conf
+    # update clxnode.conf and comment out the line for backend address 
+    #sed -i "/BACKEND_ADDR/s/^/#/" /etc/clustrix/clxnode.conf
 
-	# hack the planet and add a loop in clustrix init file to ensure /mnt/resource is mounted:
-	#sed -i '/UPSTART_NAME=clustrix/a while ! mountpoint \/mnt\/resource ; do sleep 1; echo "wait until resource disk is mounted";  done' /etc/init.d/clustrix
+    # hack the planet and add a loop in clustrix init file to ensure /mnt/resource is mounted:
+    #sed -i '/UPSTART_NAME=clustrix/a while ! mountpoint \/mnt\/resource ; do sleep 1; echo "wait until resource disk is mounted";  done' /etc/init.d/clustrix
 
-	log "setup script completed successfully on ${HOSTNAME}."
+    log "setup script completed successfully on ${HOSTNAME}."
 }
  
 setup_cluster() 
 {
-	log "starting cluster setup on ${HOSTNAME}" 
-	#myip=`ifconfig eth0 | grep inet\ addr | awk '{print $2}' | cut -b 6-20`
-	myip="$IP_PREFIX$NODE_INDEX"
-
-	#add nodes by ip to cluster: 
-	for ((i=$NODE_INDEX+2; i<=$NUM_NODES; i++ )); do
-   		mysql -e "alter cluster add \"$IP_PREFIX$i\""
-		sleep 5
-	done
-	log "Completed cluster setup on ${HOSTNAME}"	
+    log "starting cluster setup on ${HOSTNAME}" 
+    #myip=`ifconfig eth0 | grep inet\ addr | awk '{print $2}' | cut -b 6-20`
+    myip="$IP_PREFIX$NODE_INDEX"
+    # temp
+    CLX_LICENSE='{"expiration":"2015-08-14 06:24:25","company":"clustrix","email":"ablardone@clustrix.com","person":"alex","signature":"302c021455efd14b5096a91f49e2a4b7922446ef70f10e1602141d92d14885bb9add0d6b848e97d25e63a10ea3d1"}'
+    mysql -e "SET PASSWORD FOR 'root'@'%' = PASSWORD(\"$SQLPASS\")"
+    mysql -e "set global license = \"$CLX_LICENSE\""
+    mysql -e "INSERT INTO clustrix_ui.clustrix_ui_systemproperty (name) VALUES (\"install_wizard_completed\")"
+    mysql -e "set global cluster_name = \"$CLUSTER_NAME\""
+ 
+    #add nodes by ip to cluster: 
+    for ((i=$NODE_INDEX+2; i<=$NUM_NODES; i++ )); do
+        mysql -e "alter cluster add \"$IP_PREFIX$i\""
+        sleep 5
+    done
+    log "Completed cluster setup on ${HOSTNAME}"    
 }
 
 logfile=$$.log
@@ -219,13 +225,8 @@ setup_storage
 tweek_os
 install_clx
 if [ $IS_LAST_NODE -eq 1 ] && [ $CLUSTER -eq 1 ]; then
-	log "waiting 30 secs before cluster setup"
-	sleep 30
-	setup_cluster
+    log "waiting 30 secs before cluster setup"
+    sleep 30
+    setup_cluster
 fi
 log "All Done !"
-
-
-
- 
-
